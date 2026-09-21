581a32578765bc4ba9f3c45cdf98ffeec0e25216
dan593926-eng <dan593926@gmail.com>
2026-09-21 21:50:36 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // относительный base — чтобы сайт корректно работал на GitHub Pages
-  // (например https://username.github.io/repo-name/)
-  base: "./",
-});
+  base: '/birthday-site/',
+})
